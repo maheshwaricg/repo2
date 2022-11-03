53e51dab3a74ee76cf24b680e95cb6e45f7fd371
maheshwaricg <mahigirishkumar@gmail.com>
2022-11-03 14:41:02 +0530

    first commi

--- ecommerce/swiggy/urls.py
+++ ecommerce/swiggy/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('home/',views.home)
+    path('home/',views.home),
+    path('photography/',views.photography),
+    path('epanchayath/',views.epanchayath)
+
+
 ]
--- ecommerce/swiggy/views.py
+++ ecommerce/swiggy/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'swiggytemplates/home.html')
+def photography(request):
+    return render(request,'swiggytemplates/photography.html')
+def epanchayath(request):
+    return render(request,'swiggytemplates/epanchayath.html')
+
+
